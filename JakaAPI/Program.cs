cf3aaa067abb4cb311e8f2929680e3505339b6bb
Emil Shteinberg <emilshteinberg@yandex.ru>
2023-03-01 17:33:09 +0300

    Updated project

--- JakaAPI/Program.cs
+++ JakaAPI/Program.cs
@@ -1,24 +1,23 @@
 ﻿using JakaAPI;
-using Microsoft.VisualBasic;
-using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        const string ip = "192.168.1.102";
+        const string ip = "192.168.1.100";
         const int port = 10001;
 
         JakaRobot robot = new(ip, port);
 
         string JsonCommand = JakaCommand.Build("move", new CommandParameter("speed", "30"), new CommandParameter("accel", "10"));
 
-        Console.WriteLine(JsonCommand);
+        Console.WriteLine("Command example:\n" + JsonCommand);
 
         robot.PowerOn();
 
         robot.Enable();
 
+        Console.WriteLine("Press [Enter] to exit...")
         Console.ReadLine();
     }
 }
