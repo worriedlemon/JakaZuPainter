5ff1bb0fa541cda5982883e5c9f631e730241abc
Emil Shteinberg <emilshteinberg@yandex.ru>
2023-03-01 17:30:43 +0300

    Добавьте файлы проекта.

---
+++ JakaAPI/JakaAPI/JakaCommand.cs
@@ -0,0 +1,34 @@
+﻿namespace JakaAPI
+{
+
+    public struct CommandParameter
+    {
+        public string Key { get; private set; }
+        public string Value { get; private set; }
+
+        public CommandParameter(string key, string value, bool quotes = false)
+        {
+            Key = key;
+            Value = value;
+            if (quotes) Value = $"\"{Value}\"";
+        }
+    }
+
+    public static class JakaCommand
+    {
+        public static string Build(string name, params CommandParameter[] parameters)
+        {
+            string command = $"\"cmdName\":\"{name}\"";
+            foreach (var parameter in parameters)
+            {
+                command += $",\"{parameter.Key}\":{parameter.Value}";
+            }
+            return $"{{{command}}}";
+        }
+
+        public static byte[] BuildAsByteArray(string name, params CommandParameter[] parameters)
+        {
+            return System.Text.Encoding.ASCII.GetBytes(Build(name, parameters));
+        }
+    }
+}
---
+++ JakaAPI/Program.cs
@@ -0,0 +1,24 @@
+﻿using JakaAPI;
+using Microsoft.VisualBasic;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string ip = "192.168.1.102";
+        const int port = 10001;
+
+        JakaRobot robot = new(ip, port);
+
+        string JsonCommand = JakaCommand.Build("move", new CommandParameter("speed", "30"), new CommandParameter("accel", "10"));
+
+        Console.WriteLine(JsonCommand);
+
+        robot.PowerOn();
+
+        robot.Enable();
+
+        Console.ReadLine();
+    }
+}
