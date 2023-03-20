1d37c6e8e2ec3fe11df3d0477f2b05dbc5e19c74
Emil Shteinberg <emilshteinberg@yandex.ru>
2023-03-20 17:50:16 +0300

    Project information files changed

--- PainterCore/Program.cs
+++ PainterCore/Program.cs
@@ -4,8 +4,17 @@ namespace PainterCore
 {
     internal class Program
     {
+        private void PrintLicenseInfo()
+        {
+            Console.WriteLine("\tRobot-painter  Copyright (C) 2023  E. Shteinberg, A. Skuratov\n" +
+                "\tThis program comes with ABSOLUTELY NO WARRANTY.\n" +
+                "\tThis is free software, and you are welcome to redistribute it under certain conditions.\n")
+        }
+
         static void Main(string[] args)
         {
+            PrintLicenseInfo();
+
             PaintingController paintingController = new(); 
             paintingController.Start();
 
--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 ## Description
 This is a repository for the open-source code of a Robot-painter (based ob Jaka Zu 3 roboarm).
 
+## Licensing
+This project is under **GNU GPL-3** license: this is free software to use, develop and distribute.
+
 ## Annotation
 Robot-painter is a mechanism, which should imitate a real painter with his arm and techniques. LAN connection and movement of an arm are the main themes of this codespace.
 
