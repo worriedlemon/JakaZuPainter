41bc460a8dbd38f1a1a650ab543ef411c6a4f4dd
Emil Shteinberg <emilshteinberg@yandex.ru>
2023-03-20 17:50:16 +0300

    Project information files changed

--- PainterCore/Program.cs
+++ PainterCore/Program.cs
@@ -8,7 +8,7 @@ namespace PainterCore
         {
             Console.WriteLine("\tRobot-painter  Copyright (C) 2023  E. Shteinberg, A. Skuratov\n" +
                 "\tThis program comes with ABSOLUTELY NO WARRANTY.\n" +
-                "\tThis is free software, and you are welcome to redistribute it under certain conditions.\n")
+                "\tThis is free software, and you are welcome to redistribute it under certain conditions.\n");
         }
 
         static void Main(string[] args)
